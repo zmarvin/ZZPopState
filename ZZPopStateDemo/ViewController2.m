571058d7c246a1d59dda70ac9b3d23b5dd42f0f2
zmarvin <seeforward@zmarvin.com>
2021-02-07 21:56:57 +0800

    first commit

--- ZZPopStateDemo/ViewController2.m
+++ ZZPopStateDemo/ViewController2.m
@@ -18,7 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     self.view.backgroundColor = [UIColor blueColor];
-    self.zz_popSateDisabled = YES;
+//    self.zz_popSateDisabled = YES;
 }
 
 - (void)viewWillAppear:(BOOL)animated{
